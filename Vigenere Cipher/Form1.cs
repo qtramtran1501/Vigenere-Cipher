1fc0be896dc1a2390385dec74b4df24b92920af2
vanhe1602@gmail.com <72864584+anhvan1602@users.noreply.github.com>
2022-03-28 14:40:06 +0700

    Add project files.

---
+++ Vigenere Cipher/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vigenere_Cipher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //encryption function
+
+        public void vigenereEncrypt (ref StringBuilder s, string key)
+        {
+            for (int i = 0; i < s.Length; i++) s[i] = Char.ToUpper(s[i]);
+            key = key.ToUpper();
+            int j = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (Char.IsLetter(s[i]))
+                {
+                    s[i] = (char)(s[i] + key[j] - 'A');
+                    if (s[i] > 'Z') s[i] = (char)(s[i] - 'Z' + 'A' - 1);
+                }
+                j = j + 1 == key.Length ? 0 : j + 1;
+            }
+        }
+
+        public void VigenereDecrypt (ref StringBuilder s, string key)
+        {
+            for (int i = 0; i < s.Length; i++) s[i] = Char.ToUpper(s[i]);
+            key = key.ToUpper();
+            int j = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (Char.IsLetter(s[i]))
+                {
+                    s[i] = s[i] >= key[j] ?
+                              (char)(s[i] - key[j] + 'A') :
+                              (char)('A' + ('Z' - key[j] + s[i] - 'A') + 1);
+                }
+                j = j + 1 == key.Length ? 0 : j + 1;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder s = new StringBuilder(textBox6.Text);
+            String key = textBox5.Text;
+            VigenereDecrypt(ref s, key);
+            textBox4.Text = Convert.ToString(s);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder s = new StringBuilder(textBox1.Text);
+            string key = textBox2.Text;
+            vigenereEncrypt(ref s, key);
+            textBox3.Text = Convert.ToString(s);
+        }
+
+       
+    }
+}
